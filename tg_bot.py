930cad2c7738692624ca6d673df0c92ecd6b1e07
DariaAlexandrova2009 <dalexa12052009@gmail.com>
2024-07-23 19:19:07 +0400

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -7,12 +7,12 @@ from dotenv import load_dotenv
 import argparse
 
 
-def send_photos(delay):
+def send_photos(delay, path, telegram_bot, telegram_chat_id):
     while True:
-        for file in os.listdir(DIRECTORY):
-            send_file(os.path.join(DIRECTORY,file),bot,tg_chat_id)
+        for file in os.listdir(path):
+            send_file(os.path.join(path,file),telegram_bot,telegram_chat_id)
             time.sleep(delay)
-        random.shuffle(os.listdir(DIRECTORY))
+        random.shuffle(os.listdir(path))
     
 
 if __name__=='__main__':
@@ -25,4 +25,4 @@ if __name__=='__main__':
     )
     parser.add_argument('-p', '--delay', help='Укажите время ожидания', default=3600, type=int)
     args = parser.parse_args()
-    send_photos(args.delay)
+    send_photos(args.delay,DIRECTORY,bot,tg_chat_id)
